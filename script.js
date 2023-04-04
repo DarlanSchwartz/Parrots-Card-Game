02939509aa2fc121758e1e28a5b8a9fd3f621352
Darlan Schwartz <darlanswtz@gmail.com>
2023-04-04 04:26:13 -0300

    Toda funcionalidade pronta exceto os bônus

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*let quantity = prompt("Com quantas cartas você quer jogar? (4 a 14)");
+let quantity = prompt("Com quantas cartas você quer jogar? (4 a 14)");
 
 while (
     Number(quantity) < 4 ||
@@ -7,14 +7,67 @@ while (
   ) {
     quantity = prompt("Seu número deve ser par e entre 4 e 14");
   }
-*/
 
-let isSelecting;
-let firstCard;
-let secondCard;
+
+let isFlipping = false;
+let firstCard = null;
+let secondCard = null;
+let turnAmount = 0;
+let timePlaying = 0;
+
+// Array de todas as cartas duplicadas para que eu possa usar o slice, cortar no meio, e não faltar cartas..
+// porque o numero de cartas sempre vai ser par e 
+// o slice vai me retornar as cartas de 0 até a quantidade de cartas selecionada
+let avaibleCards = [
+    "./img/parrot1",
+    "./img/parrot1",
+    "./img/parrot2",
+    "./img/parrot2",
+    "./img/parrot3",
+    "./img/parrot3",
+    "./img/parrot4",
+    "./img/parrot4",
+    "./img/parrot5",
+    "./img/parrot5",
+    "./img/parrot6",
+    "./img/parrot6",
+    "./img/parrot7",
+    "./img/parrot7",
+  ];
+
+function Setup()
+{
+    // Pegar o container das cartas
+    let cardsContainer = document.querySelector('.cardsContainer');
+    // Pegar as cartas disponiveis e cortar essa array baseado na quantidade de cartas selecionada no inicio
+    let cardsToShuffle = avaibleCards.slice(0, quantity);
+    // Embaralhar essa array de cartas para embaralhar
+    cardsToShuffle.sort(comparador);
+
+    for (let i = 0; i < cardsToShuffle.length; i++) {
+        cardsContainer.innerHTML += 
+            `<div class="card back-face" onclick="FlipCard(this)">
+                <div class="front-face face">
+                    <img src="./img/back.png" alt="">
+                </div>
+                <div class="back-face face">
+                    <img src="${cardsToShuffle[i]}.gif" alt="">
+                </div>
+            </div>
+            `;
+      }
+}
+
+Setup();
 
 function FlipCard(card)
 {
+    if(isFlipping === true)
+    {
+        console.log("Cannot flip another card while other is flipping");
+        return;
+    }
+    
     if(card.classList.contains("complete"))
     {
         return;
@@ -22,8 +75,11 @@ function FlipCard(card)
     
     if(firstCard == null)
     {
+        isFlipping = true;
+        console.log('isFlipping first card');
         firstCard = card;
         card.classList.toggle('back-face');
+        turnAmount ++;
     }
     else
     {
@@ -34,40 +90,62 @@ function FlipCard(card)
         else
         {
             secondCard = card;
+            card.classList.toggle('back-face');
+            isFlipping = true;
+            console.log('isFlipping second card');
+            turnAmount ++;
 
             if(CompareCards() == true)
             {
                 firstCard.classList.add("complete");
                 secondCard.classList.add("complete");
+                firstCard = null;
+                secondCard = null;
             }
-
-            card.classList.toggle('back-face');
+            else
+            {
+                console.log('start unfliping both cards');
+                setTimeout(function () {
+                    firstCard.classList.add('back-face');
+                    secondCard.classList.add('back-face');
+                    firstCard = null;
+                    secondCard = null;
+                    isFlipping = false;
+                    console.log('end unfliping cards');
+                  }, 1000);
+            }
 
             if(CheckCompletion() == true)
             {
-                alert("Fim de jogo!");
+                alert("Você ganhou em:" + turnAmount + "jogadas!");
             }
         }
-    } 
+    }
+
+    setTimeout(function () {
+        isFlipping = false;
+      }, 1000);
 }
+
 
 function CompareCards()
 {
-    return firstCard.id === secondCard.id;
+    if(firstCard.querySelector(".back-face.face img").src == secondCard.querySelector(".back-face.face img").src)
+    {
+        return true;
+    }
+
+     return false;
 }
 
 function CheckCompletion(){
-    const allCards = document.querySelectorAll(".card");
 
-    let completo = true;
+    const allCards = document.querySelectorAll('.card');
+    const completo = [...allCards].every(carta => carta.classList.contains('complete'));
+    return completo;
+}
 
-    allCards.forEach(allCards => {
-        if (!allCards.classList.contains('complete')) {
-          // Se uma carta não estiver completa, marca completo como false e encerra o loop
-          completo = false;
-          return false;
-        }
-      });
-
-      return true;
+function comparador() 
+{ 
+    return Math.random() - 0.5; 
 }
